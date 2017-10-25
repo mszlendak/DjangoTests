9e772dd16990e7d6d8d7efc207adfea50db443ee
rolan4414 <mateusz.szlendak12@gmail.com>
2017-10-25 16:48:04 +0200

    Second commit

--- functional_tessts.py
+++ functional_tessts.py
@@ -1,4 +1,19 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("Django   ", self.browser.title)
+        self.fail("Finish The test")
+
+if __name__ == "__main__":
+    unittest.main(warnings = "ignore")
